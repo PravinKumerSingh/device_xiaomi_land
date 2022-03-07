4677dd9cdc7640021865ab33d74010771366d583
ZeeLog <Danjel.Zammit@gmail.com>
2022-03-07 15:38:12 +0100

    land: Update Perf stack from LA.UM.9.6.2.r1-04200-89xx.0
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -326,13 +326,6 @@ PRODUCT_PACKAGES += \
     android.system.net.netd@1.1.vendor \
     netutils-wrapper-1.0
 
-# Perf
-PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@1.0.vendor \
-    vendor.qti.hardware.perf@2.0.vendor \
-    vendor.qti.hardware.perf@2.1.vendor \
-    vendor.qti.hardware.perf@2.2.vendor
-
 # Platform
 TARGET_BOARD_PLATFORM := msm8937
 TARGET_BOARD_SUFFIX := _64
@@ -340,7 +333,8 @@ TARGET_BOARD_SUFFIX := _64
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
-    android.hardware.power-service-qti
+    android.hardware.power-service-qti \
+    vendor.qti.hardware.perf@2.2.vendor
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.xml \
