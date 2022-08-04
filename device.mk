835419e987a1149e5b5ab70b9a68e5090e3201de
ZeeLog <Danjel.Zammit@gmail.com>
2022-08-04 11:39:24 +0000

    land: Update perf blobs from LA.UM.8.6.2.r1-08400-89xx.0
    * Update perf (configs) blobs from LA.UM.9.6.2.r1-04200-89xx.0
    * Drop system side blobs
    * Build vendor.qti.hardware.perf@1.0 from source
    * Build vendor.qti.hardware.perf@2.1- 2.2 from source
    * Add missing perf blobs
    
    Signed-off-by: ItoRenz <itorenz@nusantararom.org>

--- device.mk
+++ device.mk
@@ -331,7 +331,10 @@ PRODUCT_PACKAGES += \
 
 # Perf
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.perf@2.0.vendor
+    vendor.qti.hardware.perf@1.0.vendor \
+    vendor.qti.hardware.perf@2.0.vendor \
+    vendor.qti.hardware.perf@2.1.vendor \
+    vendor.qti.hardware.perf@2.2.vendor
 
 # Power
 PRODUCT_PACKAGES += \
